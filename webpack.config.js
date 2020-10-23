287461638b78a53e074a7bf94fff54bf82671a26
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-10-23 14:27:33 -0700

    Installation of artillery

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  entry: path.join(__dirname + "client/index.jsx"),
+  entry: path.join(__dirname + '/client/index.jsx'),
   mode: "development",
   module: {
-    rule: [
+    rules: [
       {
-        tests: /\.(js|js)$/,
+        test: /\.(js|js|jsx)$/,
         exclude: /node_modules/,
         loader: "babel-loader",
         options: { presets: ["@babel/preset-env", "@babel/preset-react"]}
